cef62fcf3422bb101573257fcedc5a27b8a8c1e9
Poorna Kamat <kamatpoorna@gmail.com>
2023-12-27 23:43:59 +0530

    fix: added onDragEnd function to change the state of data

--- src/App.js
+++ src/App.js
@@ -5,14 +5,44 @@ import initialData from "./data/initial-data";
 import Column from "./components/TaskList/Column";
 
 function App() {
-  const [state] = useState(initialData);
+  const [store, setStore] = useState(initialData);
 
-  return state.columnOrder.map((columnId) => {
-    const column = state.columns[columnId];
-    const tasks = column.taskIds.map((taskId) => state.tasks[taskId]);
+  return store.columnOrder.map((columnId) => {
+    const column = store.columns[columnId];
+    const tasks = column.taskIds.map((taskId) => store.tasks[taskId]);
+
+    const handleDragDrop = (result) => {
+      // console.log(result);
+      const { source, destination, draggableId } = result;
+      if (!destination) return;
+      if (source.droppableId === destination.droppableId && source.index === destination.index) {
+        return;
+      }
+
+      const column = store.columns[source.droppableId];
+      const newTaskIds = Array.from(column.taskIds);
+
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+
+      const newColumn = {
+        ...column,
+        taskIds: newTaskIds,
+      };
+
+      const newStore = {
+        ...store,
+        columns: {
+          ...store.columns,
+          [newColumn.id]: newColumn,
+        },
+      };
+
+      setStore(newStore);
+    };
 
     return (
-      <DragDropContext onDragEnd={() => {}}>
+      <DragDropContext onDragEnd={handleDragDrop}>
         <div>
           <Column key={column.id} column={column} tasks={tasks} />
         </div>
