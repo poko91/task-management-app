5d0a19d9ef82fa1786250e5f16e225ecb76e9cd3
Poorna Kamat <82212882+poko91@users.noreply.github.com>
2023-12-27 15:19:22 +0530

    Created basic task list (#1)
    * feat: added initial data
    
    * feat: added basic task list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import "./index.css";
+import initialData from "./data/initial-data";
+import Column from "./components/TaskList/Column";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
+  const [state] = useState(initialData);
+
+  return state.columnOrder.map((columnId) => {
+    const column = state.columns[columnId];
+    const tasks = column.taskIds.map((taskId) => state.tasks[taskId]);
+
+    return (
+      <div>
+        <Column key={column.id} column={column} tasks={tasks} />
+      </div>
+    );
+  });
 }
 
 export default App;
