b55facf1966b43b53b9b332d9a7c2b8c011e9658
Poorna Kamat <kamatpoorna@gmail.com>
2023-12-28 11:38:41 +0530

    feat: added Pages - Board

--- src/App.js
+++ src/App.js
@@ -1,54 +1,11 @@
-import { useState } from "react";
-import "./index.css";
-import { DragDropContext } from "react-beautiful-dnd";
-import initialData from "./data/initial-data";
-import Column from "./components/TaskList/Column";
+import Board from "./pages/Board";
 
-function App() {
-  const [store, setStore] = useState(initialData);
-
-  return store.columnOrder.map((columnId) => {
-    const column = store.columns[columnId];
-    const tasks = column.taskIds.map((taskId) => store.tasks[taskId]);
-
-    const handleDragDrop = (result) => {
-      // console.log(result);
-      const { source, destination, draggableId } = result;
-      if (!destination) return;
-      if (source.droppableId === destination.droppableId && source.index === destination.index) {
-        return;
-      }
-
-      const column = store.columns[source.droppableId];
-      const newTaskIds = Array.from(column.taskIds);
-
-      newTaskIds.splice(source.index, 1);
-      newTaskIds.splice(destination.index, 0, draggableId);
-
-      const newColumn = {
-        ...column,
-        taskIds: newTaskIds,
-      };
-
-      const newStore = {
-        ...store,
-        columns: {
-          ...store.columns,
-          [newColumn.id]: newColumn,
-        },
-      };
-
-      setStore(newStore);
-    };
-
-    return (
-      <DragDropContext onDragEnd={handleDragDrop}>
-        <div>
-          <Column key={column.id} column={column} tasks={tasks} />
-        </div>
-      </DragDropContext>
-    );
-  });
-}
+const App = () => {
+  return (
+    <div>
+      <Board />
+    </div>
+  );
+};
 
 export default App;
