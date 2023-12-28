7a8476d1e6ee7c3433cbe64ebeadff04426f0648
Poorna Kamat <kamatpoorna@gmail.com>
2023-12-28 12:33:20 +0530

    fix: modified board page and created new component for TaskList

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,82 +1,34 @@
-import { useState } from "react";
-import { DragDropContext } from "react-beautiful-dnd";
-import initialData from "../data/initial-data";
-import Column from "../components/TaskList/Column";
+import TaskList from "../components/TaskList/TaskList";
 import styled from "styled-components";
+import AddTask from "../components/CreateTask/AddTask";
 
 const Container = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const Title = styled.h1`
+  font-size: 1.2rem;
+  padding: 10px;
+  font-family: "Poppins", sans-serif;
+  font-weight: 500;
 `;
 
 const Board = () => {
-  const [store, setStore] = useState(initialData);
-
-  const handleDragDrop = (result) => {
-    const { source, destination, draggableId } = result;
-    if (!destination) return;
-
-    const startColumn = store.columns[source.droppableId];
-    const finishColumn = store.columns[destination.droppableId];
-
-    // Dragging within the same column
-    if (startColumn === finishColumn) {
-      const newTaskIds = Array.from(startColumn.taskIds);
-      newTaskIds.splice(source.index, 1);
-      newTaskIds.splice(destination.index, 0, draggableId);
-
-      const newColumn = {
-        ...startColumn,
-        taskIds: newTaskIds,
-      };
-
-      const newStore = {
-        ...store,
-        columns: {
-          ...store.columns,
-          [newColumn.id]: newColumn,
-        },
-      };
-
-      setStore(newStore);
-    } else {
-      // Moving to a different column
-      const startTaskIds = Array.from(startColumn.taskIds);
-      startTaskIds.splice(source.index, 1);
-      const newStart = {
-        ...startColumn,
-        taskIds: startTaskIds,
-      };
-
-      const finishTaskIds = Array.from(finishColumn.taskIds);
-      finishTaskIds.splice(destination.index, 0, draggableId);
-      const newFinish = {
-        ...finishColumn,
-        taskIds: finishTaskIds,
-      };
-
-      const newStore = {
-        ...store,
-        columns: {
-          ...store.columns,
-          [newStart.id]: newStart,
-          [newFinish.id]: newFinish,
-        },
-      };
-      setStore(newStore);
-    }
-  };
-
   return (
-    <DragDropContext onDragEnd={handleDragDrop}>
-      <Container>
-        {store.columnOrder.map((columnId) => {
-          const column = store.columns[columnId];
-          const tasks = column.taskIds.map((taskId) => store.tasks[taskId]);
-          return <Column key={column.id} column={column} tasks={tasks} />;
-        })}
-      </Container>
-    </DragDropContext>
+    <Container>
+      <Wrapper>
+        <Title>Board</Title>
+        <AddTask />
+      </Wrapper>
+      <TaskList />
+    </Container>
   );
 };
 
